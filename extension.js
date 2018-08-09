c23d8af4da7f785d0e0ea479265731ac094f16e0
鲁悦顺 <yueshun.lu@qiyuesuo.com>
2018-08-09 12:50:32 +0800

    Using vue-template-compiler to get template content instead

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const prettydiff = require('prettydiff2');
+const compiler = require('vue-template-compiler');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -34,55 +35,16 @@ function beautify() {
     const firstLine = editor.document.positionAt(0);
     const lastLine = editor.document.positionAt(text.length - 1);
     selection = new vscode.Range(firstLine.line, firstLine.character, lastLine.line, lastLine.character);
-
-    let newTextArr = [];
-    ['html', 'js', 'css'].forEach(function(type) {
-        let newText;
-        newText = replaceText(text, type);
-        return newTextArr.push(newText);
+    let parse = compiler.parseComponent(text);
+    let configuration = vscode.workspace.getConfiguration('vueTemplateBeautify');
+    let beautifiedText = prettydiff({
+        source: parse.template.content,
+        quoteConvert: configuration['quoteConvert'],
+        wrap: configuration['wrap'],
+        force_attribute: configuration['force_attribute'],
+        mode: 'beautify'
     });
-    editor.edit(edit => edit.replace(selection, newTextArr.join('\n')));
+    text = text.slice(0, parse.template.start) + '\n' + beautifiedText + '\n' + text.slice(parse.template.end);
+    editor.edit(edit => edit.replace(selection, text));
     // Display a message box to the user
-}
-function replaceText(text, type) {
-    let annotationRex, beautifiedText, beautify, contentRex, regObj, typeArr, typeContent, typeText, typeTextCon;
-    regObj = {
-        html: /(<!--\s*)?<template(\s|\S)*>(\s|\S)*<\/template>(\s*-->)?/gi,
-        css: /(<!--\s*)?<style(\s|\S)*>(\s|\S)*<\/style>(\s*-->)?/gi,
-        js: /(<!--\s*)?<script(\s|\S)*>(\s|\S)*<\/script>(\s*-->)?/gi
-    };
-    beautify = {
-        html: prettydiff
-    };
-    if (!regObj[type].exec(text)) {
-        return '';
-    }
-    regObj[type].lastIndex = 0;
-    typeText = regObj[type].exec(text)[0];
-    if (!typeText) {
-        return '';
-    }
-    contentRex = />(\s|\S)*<\//g;
-    typeTextCon = contentRex.exec(typeText)[0];
-    typeContent = typeTextCon.substring(1).substr(0, typeTextCon.length - 3);
-    typeArr = typeText.split(typeContent);
-    if (type === 'html') {
-        annotationRex = /^(<!--)(\s|\S)*(-->)$/gi;
-        if (annotationRex.test(typeText)) {
-            return typeText;
-        }
-
-        let configuration = vscode.workspace.getConfiguration('vueTemplateBeautify');
-        beautifiedText = beautify[type]({
-            source: typeContent,
-            quoteConvert: configuration['quoteConvert'],
-            wrap: configuration['wrap'],
-            force_attribute: configuration['force_attribute'],
-            mode: 'beautify'
-        });
-
-        return typeArr[0] + '\n' + beautifiedText + '\n' + typeArr[1];
-    } else {
-        return typeText;
-    }
 }
